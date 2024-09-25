776fa555df6b8948d5faa2c5962a9c09b653df44
451suman <sumanmushyakhwo@gmail.com>
2024-09-25 14:54:54 +0545

    admin.py

--- bazar_app/admin.py
+++ bazar_app/admin.py
@@ -1,3 +1,4 @@
+from django.utils.html import format_html
 from django.contrib import admin
 
 from bazar_app.models import Category, Product, Tag, Contact
@@ -5,9 +6,42 @@ from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here .
 
-admin.site.register([Category, Tag, Contact])
+admin.site.register([Category, Tag])
+
 
 class ProductAdmin(SummernoteModelAdmin):
-    summernote_fields = ('description')
+    summernote_fields = "description"
+    list_display = (
+        "title",
+        "category",
+        "price",
+        "stock",
+        "status",
+        "published_at",
+        "featured_image_tag",
+    )
+    list_filter = ("status", "category", "published_at")
+    search_fields = ("title", "category__name", "tag__name")
+    ordering = ("-published_at",)
+    list_per_page = 3
+
+    def featured_image_tag(self, obj):
+        if obj.featured_image:
+            return format_html(
+                '<img src="{}" style="width: 50px; height: 50px;" />'.format(
+                    obj.featured_image.url
+                )
+            )
+        return "No Image"
+
+    featured_image_tag.short_description = "Image Preview"
+
 
 admin.site.register(Product, ProductAdmin)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "subject", "message")
+    ordering = ("-created_at",)
+
+admin.site.register(Contact, ContactAdmin)
